4fb6cbbe38abbc1e32883c1b0cd560b89ee43fa8
Ahmed Zaied <azaied7550@gmail.com>
2017-10-11 14:31:02 -0400

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,18 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        // Changed data type from int to double
+                double x, y, z;
+
+                x = 5;
+                y = 9.5;
+
+                z = x * y;
+
+                System.out.println("The product is " + z);
+            }
+        }
+
